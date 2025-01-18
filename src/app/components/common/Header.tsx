b52ba4b50c12cac79cd9fce7b61f987c3a8db16d
2Zerozero <hawgae@gmail.com>
2025-01-18 18:52:54 +0900

    [feat] Header 이미지 priority 누락

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
     <div className="w-full flex items-center justify-center border-b border-slate-200">
       <Link href="/" className="flex items-center justify-start w-[1200px] h-[60px]">
         {/* 로고 */}
-        <Image src="/logo/Logo.svg" alt="logo" width={152} height={40} className="hidden sm:block" />
-        <Image src="/logo/LogoSmall.svg" alt="logo" width={71} height={40} className="block sm:hidden" />
+        <Image src="/logo/Logo.svg" alt="logo" width={152} height={40} priority className="hidden sm:block" />
+        <Image src="/logo/LogoSmall.svg" alt="logo" width={71} height={40} priority className="block sm:hidden" />
       </Link>
     </div>
   );
