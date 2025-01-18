de069f7095f0cad3e17dcaad292ada7ecc7013f4
2Zerozero <hawgae@gmail.com>
2025-01-18 17:08:16 +0900

    [feat] Header 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import { PlusIcon } from '@heroicons/react/16/solid';
 import { Button } from './components/common/Button';
 import { getTodos } from './lib/api/todos';
+import Header from './components/common/Header';
 
 export default async function Home() {
   const todos = await getTodos();
 
   return (
-    <div>
-      <h1>Todo List</h1>
-      {todos.map((todo) => (
-        <div key={todo.id}>{todo.name}</div>
-      ))}
-      <Button label="추가하기" icon={<PlusIcon width={20} height={20} />} />
+    <div className="flex flex-col items-center justify-center">
+      <Header />
+
+      <div className="flex items-center justify-center w-[1200px] h-[60px]">
+        {todos.map((todo) => (
+          <div key={todo.id}>{todo.name}</div>
+        ))}
+        <Button label="추가하기" icon={<PlusIcon width={20} height={20} />} />
+      </div>
     </div>
   );
 }
